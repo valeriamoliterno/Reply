e13a1817475e4e9f70a4846b8c3a263ca58ac19c
Sisa <elysatrento@gmail.com>
2023-03-09 19:21:51 +0100

    dg0

--- sol01.cpp
+++ sol01.cpp
@@ -13,13 +13,94 @@ ofstream out("output.txt");
 int C, R, S;
 vector<int> len_S;
 vector<vector<int>> matrix;
+vector<vector<bool>> visited;
 set<pair<int,int>> wormhole;
+vector<vector<string>> paths;
 
-typedef uint64_t ll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef pair<ll, int> li;
-typedef vector<vector<int>> vvi;
+string savepath(int snake, int cur_c, int cur_r, int next_c, int next_r){
+
+        // movimento
+        if ((next_c == cur_c) && (next_r== cur_r-1)){
+            paths[snake].push_back("U");
+        }else if ((next_c == cur_c) && (next_r== cur_r+1)){
+            paths[snake].push_back("D");
+        }else if ((next_c == cur_c-1) && (next_r== cur_r)){
+            paths[snake].push_back("L");
+        }else if ((next_c == cur_c+1) && (next_r== cur_r)){
+            paths[snake].push_back("R");
+        } else {
+            //wormhole
+            paths[snake].push_back(to_string(next_r));  // occhio che sono da leggere al contrario
+            paths[snake].push_back(to_string(next_c));
+        }
+
+}
+
+pair<int,int> choosestart(){
+    return make_pair(rand()%C,rand()%R);
+    //sistemare
+}
+
+vector<pair<int,int>> choosenext(int snake, int c, int r, int l){
+    vector<pair<int,int>> papabili;
+    // papabili.push_back(make_pair(1,2));
+    // int maxPath;
+    // if (l==1){
+    //     maxPath=max({matrix[r-1][c]*(visited[r-1][c] ? -1 : 1), 
+    //         matrix[r+1][c]*(visited[r+1][c] ? -1 : 1),  
+    //         matrix[r][c-1]*(visited[r][c-1] ? -1 : 1),  
+    //         matrix[r][c+1]*(visited[r][c+1] ? -1 : 1)});
+    // }
+    if (!visited[c][r+1]){
+        papabili.push_back(make_pair(c,r+1));
+    }    
+    if (!visited[c][r-1]){
+        papabili.push_back(make_pair(c,r-1));
+    }
+    if (!visited[c+1][r]){
+        papabili.push_back(make_pair(c+1,r));
+    }
+    if (!visited[c-1][r]){
+        papabili.push_back(make_pair(c-1,r));
+    }
+    
+    //todo
+//vedi come scegliere il buco nero
+//ritorna classifica dei  vicini POSSIBILI con vec<pair<int>>
+    return papabili;
+}
+
+bool movefrom(int snake, int c, int r, int l){
+    // [c,r] gia' visitata e sistemata
+    if (l==0) return true;
+
+    bool successful=false;
+    int count=0;
+    vector<pair<int,int>> next_cells = choosenext(snake, c, r, l);   //
+    while (!successful && count < next_cells.size()){
+        int next_c= next_cells[count].first;
+        int next_r= next_cells[count].second;
+
+        if(matrix[next_c][next_r]!=-1){     // atterri in un punto normale
+            visited[next_c][next_r] = true;
+        }
+        savepath(snake, c, r, next_c, next_r);
+        bool successful = movefrom(snake, next_c, next_r, l-1);
+        count++;
+        if (!successful){
+            //backtrack
+            visited[next_c][next_r] = false;
+            paths[snake].pop_back();
+            if (matrix[c][r]==-1){
+                // distinguere buchi
+                paths[snake].pop_back();
+            }
+
+        }
+    }
+
+    return successful;
+}
 
 int main(int argc, char *argv[]) {
     ifstream in;
@@ -51,8 +132,14 @@ int main(int argc, char *argv[]) {
 
 
     for (int snake=0; snake < S; snake++){
+        pair<int,int> start = choosestart();
+        movefrom(snake,start.first,start.second,len_S[snake]-1);
         
     }
 
 
+
+
+
+    in.close(); out.close();
 }
